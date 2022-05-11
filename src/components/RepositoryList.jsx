b7d49a11e51495f3c5726245fb9b9ab9306c491f
Felipe Nogueira <felipenogueira.94@gmail.com>
2022-05-10 21:09:01 -0300

    final app repos without layout

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,11 +2,6 @@ import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/'
-}
 
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
@@ -22,10 +17,9 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     );
